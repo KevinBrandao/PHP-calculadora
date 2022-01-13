ef9501559aa034a8f6ea6764d1de598441591085
KevinBrandao <kevenbrandaoms@outlook.com>
2022-01-13 14:32:26 -0300

    Calculadora PHP

---
+++ calculadora.php
@@ -0,0 +1,23 @@
+<?php
+
+$form1 = $_POST['form1'];
+$form2 = $_POST['form2'];
+$operador = $_POST['Operador'];
+
+if ($operador == "+"){
+    $somar = $form1 + $form2;
+    echo $somar; 
+}
+if ($operador == "-"){
+    $subtrair = $form1 - $form2;
+    echo $subtrair;
+}
+if ($operador == "/"){
+    $dividir = $form1 / $form2;
+    echo $dividir;
+}
+if ($operador == "*"){
+    $mult = $form1 * $form2;
+    echo $mult;
+}
+?>
